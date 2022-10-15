d348c5c9ac002011085427bb90d6f22555069393
egorKazit <egorprostoy@gmail.com>
2022-10-15 16:32:24 +0300

    Create folders if not exists

--- src/main/java/org/telclin/client/TelegramClientLogInfo.java
+++ src/main/java/org/telclin/client/TelegramClientLogInfo.java
@@ -1,9 +1,13 @@
 package org.telclin.client;
 
+import java.io.File;
+import java.nio.file.Path;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NonNull;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Telegram client log info
@@ -18,4 +22,19 @@ public class TelegramClientLogInfo {
     final int logConsoleLevel = 0;
     @Builder.Default
     final int logFileLevel = 5;
+
+    @Contract(" -> new")
+    public static @NotNull TelegramClientLogInfoBuilder builder() {
+        return new TelegramClientLogInfoExtendedBuilder();
+    }
+
+    public static class TelegramClientLogInfoExtendedBuilder extends TelegramClientLogInfoBuilder {
+
+        public TelegramClientLogInfo build() {
+            var logInfo = super.build();
+            var parentFolder = Path.of(logInfo.logFile).getParent().toFile();
+            if (!parentFolder.exists()) parentFolder.mkdirs();
+            return logInfo;
+        }
+    }
 }
